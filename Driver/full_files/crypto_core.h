5278a2d13e65a35747973e4e0dec1e90c5382ebd
TommiTerza <118925910+TommiTerza@users.noreply.github.com>
2024-01-18 18:54:56 +0100

    Add files via upload

---
+++ Driver/full_files/crypto_core.h
@@ -0,0 +1,11 @@
+#ifndef HW_CRYPTO_CORE_H
+#define HW_CRYPTO_CORE_H
+
+#include "qom/object.h"
+
+DeviceState *crypto_core_create(hwaddr);
+
+#endif
+
+
+/**License: [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)**/
